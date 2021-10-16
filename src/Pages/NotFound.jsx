1f19739dd80ee2688822b62cee4cd9f4f6a49529
aarrieth <alfonsoluisarrieta253@gmail.com>
2021-10-16 11:42:24 -0500

    [notFound page]

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import HiperLink from '@components/HiperLink';
+import Layout from '@containers/Layout';
+import Title from '@components/Title';
+
+import '@styles/NotFound.scss';
 
 const NotFound = () => {
 	return (
-		<div>
-			<h1>404 PAGE NOT FOUND</h1>
-		</div>
+		<Layout className="notFound">
+			<Layout className="notFound-inner">
+				<Title id="title" className="notFound-title">
+					<span>404</span> <br /> PAGE NOT FOUND
+				</Title>
+				<HiperLink>
+					Click para regresar a la página principal
+				</HiperLink>
+			</Layout>
+		</Layout>
 	);
 };
 
