4a74413ebd5ae11c7683b41d7793478c8edbe042
aarrieth <alfonsoluisarrieta253@gmail.com>
2021-10-21 14:23:12 -0500

    [toggle menu]

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Header from '@containers/Header';
-import ProductDetail from '../containers/ProductDetail';
-import Checkout from '@containers/Checkout';
+
 const Home = () => {
 	return (
 		<>
 			<Header />
-			<Checkout />
 		</>
 	);
 };
--- src/containers/Menu.jsx
+++ src/containers/Menu.jsx
@@ -11,9 +11,7 @@ const Menu = ({ defaultToggled = false }) => {
 	return (
 		<Layout className="desktop-menu">
 			<UnorderedList className="desktop-menu--inner">
-				<HiperLink path="/" className="title">
-					My orders
-				</HiperLink>
+				<HiperLink path="/">My orders</HiperLink>
 				<HiperLink path="/">My account</HiperLink>
 				<HiperLink path="/">Sign out</HiperLink>
 			</UnorderedList>
